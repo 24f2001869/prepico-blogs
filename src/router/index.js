afe2462e22ee5bfe430d4478e21d5fd443809a34
24f2001869 <24f2001869@ds.study.iitm.ac.in>
2026-01-11 15:09:13 +0530

    Update index.js

--- src/router/index.js
+++ src/router/index.js
@@ -1,1 +1,30 @@
+import { createRouter, createWebHistory } from 'vue-router'
+import HomeView from '../views/HomeView.vue'
+import AdminView from '../views/AdminView.vue'
+import BlogDetailView from '../views/BlogDetailView.vue'
 
+const routes = [
+  {
+    path: '/',
+    name: 'home',
+    component: HomeView
+  },
+  {
+    path: '/admin',
+    name: 'admin',
+    component: AdminView
+  },
+  {
+    path: '/blog/:id',
+    name: 'blog-detail',
+    component: BlogDetailView,
+    props: true
+  }
+]
+
+const router = createRouter({
+  history: createWebHistory(),
+  routes
+})
+
+export default router
